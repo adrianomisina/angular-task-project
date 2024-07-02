87fa1123e4a65e905c8142b4ffb7e85bf0bda037
adrianomisina <adriano.misina@gmail.com>
2024-07-02 05:52:55 -0300

    feat: getting Started with Dependency Injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,14 +14,17 @@ import { type NewTaskData } from './task/task.model';
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
   isAddingTask: boolean = false;
 
+  constructor(private taskService: TasksService) {}
+
   get selectedUserTasks() {
-    return 
+    return this.taskService.getUserTasks(this.userId)
   }
 
   onCompliteTaskDelete(id: string) {
-
+    return 
   }
 
   onStartAddTask() {
