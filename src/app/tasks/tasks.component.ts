161f58168bf9fe9c46adb2131efead067ce2029f
adrianomisina <adriano.misina@gmail.com>
2024-07-02 05:31:57 -0300

    getting started with service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,55 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
 import { type NewTaskData } from './task/task.model';
 
 @Component({
-    selector: 'app-tasks',
-    standalone: true,
-    templateUrl: './tasks.component.html',
-    styleUrls: ['./tasks.component.css'],
-    imports: [CommonModule, TaskComponent, NewTaskComponent]
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+  imports: [CommonModule, TaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask: boolean = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
-
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return 
   }
 
   onCompliteTaskDelete(id: string) {
-    this.tasks = this.tasks.filter((task)=> {
-      task.id !== id
-    })
+
   }
 
   onStartAddTask() {
@@ -61,13 +33,7 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
+
     this.isAddingTask = false;
   }
 }
